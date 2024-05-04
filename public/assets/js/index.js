2c0e6915f5009da7745342ae1980c1ff6ccf6fde
Crlyhdfrd <152818336+Crlyhdfrd@users.noreply.github.com>
2024-05-03 19:04:33 -0500

    fixed error with porting

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,9 +1,10 @@
 let noteForm = document.querySelector('.note-form');
 let noteTitle = document.querySelector('.note-title');
-let noteText = document.querySelector('.note-textarea'); 
-let saveNoteBtn = document.querySelector('.save-note'); 
-let newNoteBtn = document.querySelector('.new-note');
-let noteList = document.querySelectorAll('.list-container .list-group');
+let noteText = document.querySelector('.note-textarea');
+let saveNoteBtn = document.querySelector('.save-note');
+ let newNoteBtn = document.querySelector(".new-note");
+  let clearBtn = document.querySelector(".clear-btn");
+  let noteList = document.querySelectorAll(".list-container .list-group");
 
 if (window.location.pathname === '/notes') {
   noteForm = document.querySelector('.note-form');
--- server.js
+++ server.js
@@ -1,16 +1,59 @@
 const PORT = process.env.PORT || 3000;
-const express = require('express');
+const express = require("express");
 const app = express();
-const path = require('path');
-const fs = require('fs');
-const apiRoutes = require('./routes/apiRoutes');
-const htmlRoutes = require('./routes/htmlRoutes');
+const path = require("path");
+const fs = require("fs");
 
-app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('public'));
-app.use('/api', apiRoutes);
-app.use('/', htmlRoutes);
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "notes.html"));
+});
+
+app.get("/api/notes", (req, res) => {
+  const filePath = path.join(__dirname, "db", "db.json");
+
+  fs.readFile(filePath, "utf8", (err, data) => {
+    if (err) {
+      return console.log(err);
+    }
+    res.json(JSON.parse(data));
+  });
+});
+
+app.post("/api/notes", (req, res) => {
+  const filePath = path.join(__dirname, "db", "db.json");
+
+  fs.readFile(filePath, "utf8", (err, data) => {
+    if (err) {
+      return console.log(err);
+    }
+
+    const notes = JSON.parse(data);
+    const newNote = req.body;
+    console.log(newNote);
+    notes.push(newNote);
+
+    fs.writeFile(filePath, JSON.stringify(notes), (err) => {
+      if (err) {
+        return console.log(err);
+      }
+      res.json(newNote);
+    });
+  });
+});
+
+app.delete("/api/notes/:id", (req, res) => {
+  const filePath = path.join(__dirname, "db", "db.json");
+
+  fs.readFile(filePath, "db", "db.json");
+});
 
 app.listen(PORT, () => {
   console.log(`App listening on PORT ${PORT}`);
